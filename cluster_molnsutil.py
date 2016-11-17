f915ecdd5ba9686a33d2e05b6f6f1fd94cc5acf1
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-16 16:11:03 -0800

    enahance wrapper API over molnsutil

--- cluster_molnsutil.py
+++ cluster_molnsutil.py
@@ -34,47 +34,56 @@ class DistributedEnsemble(ClusterParameterSweep):
     def __init__(self, model_class, parameters=None):
         ClusterParameterSweep.__init__(self, model_cls=model_class, parameters=parameters, remote_host=get_remote_host())
         self.realizations_storage_dir = None
+        self.remote_job = None
 
     def add_realizations(self, number_of_trajectories=None):
         if number_of_trajectories is None:
             raise ClusterExecutionException("Number of trajectories cannot be None.")
 
-        remote_job = self.run_async(number_of_trajectories=number_of_trajectories, add_realizations=True)
+        self.remote_job = self.run_async(number_of_trajectories=number_of_trajectories, add_realizations=True)
         print "Generating {0} realizations...".format(number_of_trajectories)
         import json
-        res = json.loads(self.get_results(remote_job, add_realizations=True))
+        res = json.loads(self.get_results(self.remote_job, add_realizations=True))
         self.realizations_storage_dir = res["realizations_directory"]
         return res
 
-    def run(self, mapper, reducer=None, aggregator=None, store_realizations=False, number_of_trajectories=None):
-        remote_job = self.run_async(mapper=mapper, reducer=reducer, aggregator=aggregator,
-                                    store_realizations=store_realizations,
-                                    number_of_trajectories=number_of_trajectories)
+    def run(self, mapper=None, reducer=None, aggregator=None, store_realizations=False, number_of_trajectories=None):
+        mapper = DistributedEnsemble.__get_mapper(mapper)
+        self.remote_job = self.run_async(mapper=mapper, reducer=reducer, aggregator=aggregator,
+                                         store_realizations=store_realizations,
+                                         number_of_trajectories=number_of_trajectories)
         print "Waiting for results to be computed..."
-        return self.get_results(remote_job)
+        return self.get_results(self.remote_job)
 
-    def mean_variance(self, mapper, realizations_storage_directory=None):
+    def mean_variance(self, mapper=None, realizations_storage_directory=None):
         """ Compute the mean and variance (second order central moment) of the function g(X) based on
         number_of_trajectories realizations in the ensemble. """
 
+        mapper = DistributedEnsemble.__get_mapper(mapper)
+
         realizations_storage_directory = self.__get_realizations_storage_directory(realizations_storage_directory)
 
-        remote_job = self.run_async(mapper=mapper, aggregator=builtin_aggregator_sum_and_sum2,
-                                    reducer=builtin_reducer_mean_variance,
-                                    realizations_storage_directory=realizations_storage_directory)
+        self.remote_job = self.run_async(mapper=mapper, aggregator=builtin_aggregator_sum_and_sum2,
+                                         reducer=builtin_reducer_mean_variance,
+                                         realizations_storage_directory=realizations_storage_directory)
         print "Waiting for results to be computed..."
-        return self.get_results(remote_job)
+        return self.get_results(self.remote_job)
 
-    def mean(self, mapper, realizations_storage_directory=None):
+    def mean(self, mapper=None, realizations_storage_directory=None):
         """ Compute the mean of the function g(X) based on number_of_trajectories realizations
             in the ensemble. It has to make sense to say g(result1)+g(result2). """
 
+        mapper = DistributedEnsemble.__get_mapper(mapper)
+
         realizations_storage_directory = self.__get_realizations_storage_directory(realizations_storage_directory)
 
-        remote_job = self.run_async(mapper=mapper, aggregator=builtin_aggregator_add, reducer=builtin_reducer_mean,
-                                    realizations_storage_directory=realizations_storage_directory)
+        self.remote_job = self.run_async(mapper=mapper, aggregator=builtin_aggregator_add, reducer=builtin_reducer_mean,
+                                         realizations_storage_directory=realizations_storage_directory)
         print "Waiting for results to be computed..."
-        return self.get_results(remote_job)
+        return self.get_results(self.remote_job)
+
+    def clean_up(self):
+        ClusterParameterSweep.clean_up(self, remote_job=self.remote_job)
 
     def __get_realizations_storage_directory(self, directory):
         if directory is not None:
@@ -82,6 +91,13 @@ class DistributedEnsemble(ClusterParameterSweep):
         if self.realizations_storage_dir is None:
             raise ClusterExecutionException("Something went wrong. Unknown realizations storage directory.")
         return self.realizations_storage_dir
+
+    @staticmethod
+    def __get_mapper(mapper):
+        if mapper is not None:
+            return mapper
+        else:
+            raise ClusterExecutionException("No default mapper available. Please specify one.")
 
 
 class ParameterSweep(DistributedEnsemble):
