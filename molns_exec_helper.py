e7227e35f98e7a633869f5a432b76c1da4028a85
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-23 17:02:40 -0800

    add logger facility'

--- molns_exec_helper.py
+++ molns_exec_helper.py
@@ -1,70 +1,70 @@
 #!/usr/bin/env python
 
 # This program executes on a cluster.
-import traceback
 import json
 import pickle
 import sys
 import os
+import logging
 
 
-def run_job(logs, cluster_exec_input_file, cluster_exec_output_file, pickled_cluster_input_file, storage_dir=None):
-        with open(logs, 'w') as stdout_fh:
-            try:
-                lib_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-                sys.path.append(lib_path)
+def run_job(logs, cluster_exec_input_file, cluster_exec_output_file, pickled_cluster_input_file, storage_dir=None,
+            logger=None):
+        try:
+            lib_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            sys.path.append(lib_path)
 
-                import molnsutil
+            import molnsutil
 
-                with open(cluster_exec_input_file, "rb") as inp:
-                    inp_obj = pickle.load(inp)
+            with open(cluster_exec_input_file, "rb") as inp:
+                inp_obj = pickle.load(inp)
 
-                number_of_trajectories = inp_obj['number_of_trajectories']
+            number_of_trajectories = inp_obj['number_of_trajectories']
 
-                if inp_obj.get('add_realizations', False):
-                    ensemble = molnsutil.DistributedEnsemble(pickled_cluster_input_file=pickled_cluster_input_file,
-                                                             qsub=True, storage_mode="Local")
-                    result = json.loads(ensemble.add_realizations(number_of_trajectories=number_of_trajectories))
+            if inp_obj.get('add_realizations', False):
+                ensemble = molnsutil.DistributedEnsemble(pickled_cluster_input_file=pickled_cluster_input_file,
+                                                         qsub=True, storage_mode="Local", logger=logger)
+                result = json.loads(ensemble.add_realizations(number_of_trajectories=number_of_trajectories))
 
-                    old_storage_dir = result["realizations_directory"]
-                    # Copy generated realizations from temporary directory to job directory.
-                    result["realizations_directory"] = molnsutil.utils.copy_generated_realizations_to_job_directory(
-                        realizations_storage_directory=old_storage_dir,
-                        store_realizations_dir=storage_dir)
+                old_storage_dir = result["realizations_directory"]
+                # Copy generated realizations from temporary directory to job directory.
+                result["realizations_directory"] = molnsutil.utils.copy_generated_realizations_to_job_directory(
+                    realizations_storage_directory=old_storage_dir,
+                    store_realizations_dir=storage_dir)
 
-                    if len(os.listdir(result["realizations_directory"])) == 0:
-                        from molnsutil.utils import jsonify
-                        raise Exception(jsonify(logs="Something went wrong while generating realizations. "
-                                        "Please check {0} for detailed logs.".format(old_storage_dir)))
+                if len(os.listdir(result["realizations_directory"])) == 0:
+                    from molnsutil.utils import jsonify
+                    raise Exception(jsonify(logs="Something went wrong while generating realizations. "
+                                    "Please check {0} for detailed logs.".format(old_storage_dir)))
 
-                    result = json.dumps(result)
+                result = json.dumps(result)
 
-                elif inp_obj.get('realizations_storage_directory', None) is not None:
-                    ensemble = molnsutil.DistributedEnsemble(pickled_cluster_input_file=pickled_cluster_input_file,
-                                                             qsub=True, storage_mode="Local")
-                    mapped_results = ensemble.qsub_map_aggregate_stored_realizations(
-                        pickled_cluster_input_file=pickled_cluster_input_file,
-                        realizations_storage_directory=inp_obj.get('realizations_storage_directory'),
-                        result_list=inp_obj.get("result_list"))
-                    result = ensemble.run_reducer(pickled_cluster_input_file=pickled_cluster_input_file,
-                                                  mapped_results=mapped_results)
+            elif inp_obj.get('realizations_storage_directory', None) is not None:
+                ensemble = molnsutil.DistributedEnsemble(pickled_cluster_input_file=pickled_cluster_input_file,
+                                                         qsub=True, storage_mode="Local", logger=logger)
+                mapped_results = ensemble.qsub_map_aggregate_stored_realizations(
+                    pickled_cluster_input_file=pickled_cluster_input_file,
+                    realizations_storage_directory=inp_obj.get('realizations_storage_directory'),
+                    result_list=inp_obj.get("result_list"))
+                result = ensemble.run_reducer(pickled_cluster_input_file=pickled_cluster_input_file,
+                                              mapped_results=mapped_results)
 
-                else:
-                    params = inp_obj['params']
-                    store_realizations = inp_obj['store_realizations']
+            else:
+                params = inp_obj['params']
+                store_realizations = inp_obj['store_realizations']
 
-                    sweep = molnsutil.ParameterSweep(pickled_cluster_input_file=pickled_cluster_input_file,
-                                                     parameters=params, qsub=True,
-                                                     storage_mode="Local")
-                    result = sweep.run(number_of_trajectories=number_of_trajectories,
-                                       store_realizations=store_realizations, progress_bar=False,
-                                       store_realizations_dir=storage_dir)
+                sweep = molnsutil.ParameterSweep(pickled_cluster_input_file=pickled_cluster_input_file,
+                                                 parameters=params, qsub=True, logger=logger, storage_mode="Local")
+                result = sweep.run(number_of_trajectories=number_of_trajectories,
+                                   store_realizations=store_realizations, progress_bar=False,
+                                   store_realizations_dir=storage_dir)
 
-                with open(cluster_exec_output_file, "w") as out:
-                    out.write("{0}".format(result))
+            with open(cluster_exec_output_file, "w") as out:
+                out.write("{0}".format(result))
 
-            except Exception as e:
-                stdout_fh.write("{0} \n\n {1}".format(e, traceback.format_exc()))
+        except Exception as e:
+            with open(logs, 'w') as stdout_fh:
+                stdout_fh.write("\n\n ----------------------------------------------------------- \n\n {0}".format(e))
 
 
 if __name__ == "__main__":
@@ -77,10 +77,15 @@ if __name__ == "__main__":
     with open(os.path.join(base_job_dir, "pid"), 'w+') as p:
         p.write(str(os.getpid()))
 
-    run_job(logs=os.path.join(base_job_dir, "molns_exec_helper_logs"),
-            cluster_exec_input_file=os.path.join(base_job_dir, "cluster-exec-input-file"),
+    log_file = os.path.join(base_job_dir, "molns_exec_helper_logs")
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("molns_exec_helper")
+    logger.addHandler(logging.FileHandler(filename=log_file))
+
+    run_job(logs=log_file, cluster_exec_input_file=os.path.join(base_job_dir, "cluster-exec-input-file"),
             cluster_exec_output_file=os.path.join(base_job_dir, "cluster-exec-output-file"),
-            storage_dir=realizations_dir,
+            storage_dir=realizations_dir, logger=logger,
             pickled_cluster_input_file=os.path.join(base_job_dir, "pickled-cluster-input-file"))
 
     with open(os.path.join(base_job_dir, "cluster-exec-job-complete"), 'w+') as comp:
